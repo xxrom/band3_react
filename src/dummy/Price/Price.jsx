ca7130f4f9911e94e8ac29e07e686699a0b7977d
Chernyshov <xpom55@mail.ru>
2019-01-29 18:52:36 +0300

    Add Rub sign

--- src/dummy/Price/Price.jsx
+++ src/dummy/Price/Price.jsx
@@ -1,14 +1,33 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
 import './Price.css';
 
-class Price extends PureComponent {
+class Price extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      priceOld: 3290,
+      priceNew: 2490,
+    };
+  }
   render() {
+    const { priceOld, priceNew } = this.state;
+    // const discount = Math.ceil(((priceOld - priceNew) / priceOld) * 100);
+    const benefit = Math.round(priceOld - priceNew);
+
     return (
       <div className="price">
-        <div className="price_old">200 руб.</div>
-        <div className="price_new">150 руб.</div>
-        <div className="price_discount">Скидка 25%</div>
+        <div className="price_old">
+          {priceOld}
+          <span className="price_currency">&#8381;</span>
+        </div>
+        <div className="price_new">
+          {priceNew}
+          <span className="price_currency">&#8381;</span>
+        </div>
+        <div className="price_benefit">Выгода {benefit} &#8381;</div>
+        {/* <div className="price_discount">Скидка {discount}%</div> */}
       </div>
     );
   }
