e6e9d579b7ecfbe3b0ef7ccd27fa23f2ddd89f9a
Chernyshov <xpom55@mail.ru>
2019-03-21 02:33:30 +0300

    + img with onLoad

--- src/dummy/Image/Image.jsx
+++ src/dummy/Image/Image.jsx
@@ -3,11 +3,49 @@ import React, { PureComponent } from 'react';
 import './Image.css';
 
 class Image extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      src: props.src,
+    };
+
+    console.log('Img', props.src);
+    if (props.src === 'main11_high_900.png') {
+      console.log('main11_high_900.png');
+      this.t = (
+        <img
+          style={props.style}
+          className={`${props.classMyName ? props.classMyName : 'image'}`}
+          // onLoad={(e) => console.log('loaded src', props.src, e)}
+          onLoad={(e) => console.log('@@@@@@@@@@@@@@@@ onLoad ', e)}
+          src={this.getSrc(props.src)}
+          alt={props.alt ? props.alt : 'img'}
+        />
+      );
+    }
+  }
+
   render() {
     const { src, style, classMyName, alt } = this.props;
 
+    return (
+      <div>
+        {this.t}
+        <img
+          style={style}
+          className={`${classMyName ? classMyName : 'image'}`}
+          // onLoad={(e) => console.log('onLoad', e)}
+          src={this.getSrc(this.state.src)}
+          alt={alt ? alt : 'img'}
+        />
+      </div>
+    );
+  }
+
+  getSrc = (src) => {
     let srcImg;
-    // Check for webp files
+
     try {
       let localSrc = 'jpg';
       if (src.indexOf('png') !== -1) {
@@ -18,15 +56,8 @@ class Image extends PureComponent {
       srcImg = require(`../../assets/${src}`);
     }
 
-    return (
-      <img
-        style={style}
-        className={`${classMyName ? classMyName : 'image'}`}
-        src={srcImg}
-        alt={alt ? alt : 'img'}
-      />
-    );
-  }
+    return srcImg;
+  };
 }
 
 export { Image };
